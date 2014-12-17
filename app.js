97e3e47dc14a7d1267d200759ebd3dc220a7a767
Joseph McCarron <joe@zendesk.com>
2014-12-17 15:29:51 -0800

    fill in form data

--- app.js
+++ app.js
@@ -2,10 +2,53 @@
 
   return {
     events: {
-      'app.activated':'doSomething'
+      'app.created':'onLoad',
+
+      'getGroups.done':'gotGroups',
+      'getLocales.done':'gotLocales'
+    },
+    // defaultState: 'mockup',
+    requests: {
+      getGroups: function() {
+        return {
+          url: '/api/v2/groups.json'
+        };
+      },
+      getLocales: function() {
+        return {
+          url: '/api/v2/locales.json'
+        };
+      }
     },
 
-    doSomething: function() {
+    onLoad: function() {
+      this.ajax('getGroups');
+
+    },
+    gotGroups: function(response) {
+      this.groups = response.groups;
+      this.ajax('getLocales');
+      
+    },
+    gotLocales: function(response) {
+      this.locales = response.locales;
+      this.showMaster();
+    },
+    showMaster: function() {
+      this.switchTo('master', {
+        groups: this.groups,
+        locales: this.locales
+      });
+    },
+    getViews: function() {
+      // 
+
+    },
+    gotViews: function() {
+
+    },
+    filter: function() {
+      
     }
   };
 
